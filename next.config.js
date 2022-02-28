5fcc9e68969185f85af96486704747f24a88cb21
Ludovico Russo <ludus.russo@gmail.com>
2022-02-28 16:28:57 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const { withSuperjson } = require("next-superjson");
 
 module.exports = withSuperjson()({
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+    loader: "cloudinary",
+    path: "https://res.cloudinary.com/dbdvy5b2z/image/upload/",
+  },
+  experimental: {
+    outputStandalone: true,
+  },
 });
