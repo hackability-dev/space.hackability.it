9779bcebdd7624dd5ad2606653d2da338949c55b
Ludovico Russo <ludus.russo@gmail.com>
2022-07-14 20:47:34 +0200

    fix: add privacy and upload navigation

--- core/layouts/Layout.tsx
+++ core/layouts/Layout.tsx
@@ -2,16 +2,7 @@ import Head from "next/head";
 import { FC } from "react";
 
 const Layout: FC<{ title?: string }> = ({ title, children }) => {
-  return (
-    <>
-      <Head>
-        <title>{title || "hackability.space"}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {children}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default Layout;
